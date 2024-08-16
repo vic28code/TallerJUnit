e749d543b7c54ee1a759dea6acc2f97f32952cc8
AlejandroSV2004 <alesvera@espol.edu.ec>
2024-08-15 20:56:57 -0500

    SECCION 1 ALEJANDRO SORNOZA

--- src/proyecto/Calculation.java
+++ src/proyecto/Calculation.java
@@ -2,13 +2,17 @@ package proyecto;
 
 public class Calculation {
 	
-	public static int findMax(int[] arr){
-	    int max = 0;
-	    for(int i = 1; i < arr.length; i++){
-	        if(max < arr[i])
-	            max = arr[i];
-	    }
-	    return max;
-	}
+	public static int findMax(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("El arreglo no puede estar vacío.");
+        }
 
+        int max = arr[0]; 
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
--- src/proyecto/TestCalculation.java
+++ src/proyecto/TestCalculation.java
@@ -5,10 +5,19 @@ import org.junit.Test;
 
 public class TestCalculation {
 	@Test
-	public static void testFindMax(){
-		assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
-		assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}) );
-		}
+    public void testFindMaxWithPositiveAndNegativeNumbers() {
+        assertEquals(4, Calculation.findMax(new int[] {1, 2, 3, 4}));
+        assertEquals(-1, Calculation.findMax(new int[] {-12, -1, -3, -4, -2}));
+        assertEquals(10, Calculation.findMax(new int[] {-10, -5, 0, 3, 10}));
+    }
 
-	
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindMaxWithEmptyArray() {
+        Calculation.findMax(new int[] {});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindMaxWithNullArray() {
+        Calculation.findMax(null);
+    }
 }
